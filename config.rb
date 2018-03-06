5338645f6cac4e658c05b0cc87a8a11983fde5ea
RonaldDijkstra <ronald@defacto.nl>
2018-03-07 00:23:00 +0100

    Install bitters, linter, livereload

--- config.rb
+++ config.rb
@@ -40,6 +40,10 @@ page '/*.txt', layout: false
 # Build-specific configuration
 # https://middlemanapp.com/advanced/configuration/#environment-specific-settings
 
+configure :development do
+  activate :livereload
+end
+
 # configure :build do
 #   activate :minify_css
 #   activate :minify_javascript
