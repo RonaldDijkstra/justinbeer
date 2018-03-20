75ba328a37a81c0885c1ffe8005fbdd4e079fe53
RonaldDijkstra <ronald@defacto.nl>
2018-03-20 22:20:22 +0100

    Add Google Maps location

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,15 +1,2 @@
-// Mobile Navigation
-$('.mobile-toggle').click(function () {
-  if ($('.main_h').hasClass('open-nav')) {
-    $('.main_h').removeClass('open-nav');
-  } else {
-    $('.main_h').addClass('open-nav');
-  }
-});
-
-$('.main_h li a').click(function () {
-  if ($('.main_h').hasClass('open-nav')) {
-    $('.navigation').removeClass('open-nav');
-    $('.main_h').removeClass('open-nav');
-  }
-});
+//= require _nav
+//= require _maps
